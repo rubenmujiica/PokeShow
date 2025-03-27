41baa47846860527d5109ef080df6055ab0e9e62
rafaPG03 <rafa.perezgar@alum.uca.es>
2025-03-27 17:32:45 +0100

    footer

--- coleccion.php
+++ coleccion.php
@@ -159,6 +159,54 @@ session_start();
             background-color:rgb(144, 7, 18); /* Un rojo más oscuro */
             transform: scale(1.05); /* Efecto sutil de agrandado */
         }
+        footer {
+        background-color: rgb(219, 57, 71);
+        padding: 15px;
+        text-align: center;
+        border-top: 2px solid white;
+        margin-top: 50px;
+        }
+        .footer-content {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 10px;
+        }
+        .footer-links a {
+            color: white;
+            text-decoration: none;
+            padding: 8px 15px;
+            background-color: black;
+            border-radius: 10px;
+            transition: transform 0.2s, background-color 0.3s;
+            border: 2px solid white;
+            margin: 5px;
+            display: inline-block;
+        }
+        .footer-links a:hover {
+            background-color: white;
+            color: black;
+            transform: scale(1.1);
+            border: 2px solid #e60012;
+        }
+        footer p {
+            color: white;
+            font-weight: bold;
+        }
+        .footer-social .social-icons {
+            display: flex;
+            justify-content: center;
+            gap: 15px;
+        }
+        .footer-social img {
+            width: 75px;
+            height: 75px;
+            object-fit: contain;
+            transition: transform 0.2s;
+        }
+        .footer-social img:hover {
+            transform: scale(1.2);
+        }
     </style>
 </head>
 <body>
@@ -199,6 +247,28 @@ session_start();
         </div>
         <button type="submit" class="boton_anadir">Añadir carta</button>
     </form>
-
+    <footer>
+    <div class="footer-content">
+        <p>&copy; 2025 - PokeShop</p>
+        <div class="footer-contact">
+            <p>Contacto: pokeshop@gmail.com   |   Teléfono: +34 999 999 999</p>
+        </div>
+        <div class="footer-links">
+            <a href="https://www.pokemon.com/es/legal/condiciones-de-uso">Términos y Condiciones</a>
+            <a href="https://www.pokemon.com/es/legal/aviso-de-privacidad">Política de Privacidad</a>
+        </div>
+        <div class="footer-social">
+            <p>Síguenos en redes sociales:</p>
+            <a href="https://www.facebook.com/PokemonOficialES?locale=es_ES" target="_blank">
+                <img src="imgs\facebook.webp" alt="Facebook" width="175" height="175">
+            </a>
+            <a href="https://www.instagram.com/pokemon/" target="_blank">
+                <img src="imgs\insta.webp" alt="Instagram" width="150" height="150">
+            </a>
+            <a href="https://x.com/Pokemon_ES_ESP" target="_blank">
+                <img src="imgs\twitter.png" alt="Twitter" width="100" height="100">
+        </div>
+    </div>
+    </footer>
 </body>
 </html>
